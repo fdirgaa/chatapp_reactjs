05b050e8743ce8fa71f421b8c0afb6007eebbbd0
Ferdi Dirgantara <ferdifordev@gmail.com>
2022-03-24 12:00:59 +0700

    set avatar interface and functionality

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,12 +1,10 @@
-const { register, login } = require("../controllers/usersController");
+const { register, login, setAvatar } = require("../controllers/usersController");
 
 const router = require("express").Router()
 
 router.post("/register", register)
 router.post("/login", login)
+router.post("/setavatar/:id", setAvatar)
 
-router.get('/register', (req, res) => {
-  res.json({message: "Sukses"})
-})
 
 module.exports = router
